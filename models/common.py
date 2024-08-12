099b77e69e414e61314a57318ad905f794fc8fdc
unknown <abrar.bd27@gmail.com>
2024-08-12 16:28:55 +0600

    updated code

--- models/common.py
+++ models/common.py
@@ -21,6 +21,7 @@ import torch
 import torch.nn as nn
 from PIL import Image
 from torch.cuda import amp
+import torch.nn.functional as F
 
 # Import 'ultralytics' package or install if missing
 try:
@@ -237,6 +238,119 @@ class C3(nn.Module):
         """Performs forward propagation using concatenated outputs from two convolutions and a Bottleneck sequence."""
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
 
+## written new functions for testing performance of CBAM
+class CBAM(nn.Module):
+
+    def __init__(self, n_channels_in, reduction_ratio, kernel_size):
+        super(CBAM, self).__init__()
+        self.n_channels_in = n_channels_in
+        self.reduction_ratio = reduction_ratio
+        self.kernel_size = kernel_size
+
+        self.channel_attention = ChannelAttention(n_channels_in, reduction_ratio)
+        self.spatial_attention = SpatialAttention(kernel_size)
+
+    def forward(self, f):
+        chan_att = self.channel_attention(f)
+        # print(chan_att.size())
+        fp = chan_att * f
+        # print(fp.size())
+        spat_att = self.spatial_attention(fp)
+        # print(spat_att.size())
+        fpp = spat_att * fp
+        # print(fpp.size())
+        return fpp
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size):
+        super(SpatialAttention, self).__init__()
+        self.kernel_size = kernel_size
+
+        assert kernel_size % 2 == 1, "Odd kernel size required"
+        self.conv = nn.Conv2d(in_channels = 2, out_channels = 1, kernel_size = kernel_size, padding= int((kernel_size-1)/2))
+        # batchnorm
+
+    def forward(self, x):
+        max_pool = self.agg_channel(x, "max")
+        avg_pool = self.agg_channel(x, "avg")
+        pool = torch.cat([max_pool, avg_pool], dim = 1)
+        conv = self.conv(pool)
+        # batchnorm ????????????????????????????????????????????
+        conv = conv.repeat(1,x.size()[1],1,1)
+        att = torch.sigmoid(conv)        
+        return att
+
+    def agg_channel(self, x, pool = "max"):
+        b,c,h,w = x.size()
+        x = x.view(b, c, h*w)
+        x = x.permute(0,2,1)
+        if pool == "max":
+            x = F.max_pool1d(x,c)
+        elif pool == "avg":
+            x = F.avg_pool1d(x,c)
+        x = x.permute(0,2,1)
+        x = x.view(b,1,h,w)
+        return x
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, n_channels_in, reduction_ratio):
+        super(ChannelAttention, self).__init__()
+        self.n_channels_in = n_channels_in
+        self.reduction_ratio = reduction_ratio
+        self.middle_layer_size = int(self.n_channels_in/ float(self.reduction_ratio))
+
+        self.bottleneck = nn.Sequential(
+            nn.Linear(self.n_channels_in, self.middle_layer_size),
+            nn.ReLU(),
+            nn.Linear(self.middle_layer_size, self.n_channels_in)
+        )
+
+
+    def forward(self, x):
+        kernel = (x.size()[2], x.size()[3])
+        avg_pool = F.avg_pool2d(x, kernel )
+        max_pool = F.max_pool2d(x, kernel)
+
+        
+        avg_pool = avg_pool.view(avg_pool.size()[0], -1)
+        max_pool = max_pool.view(max_pool.size()[0], -1)
+        
+
+        avg_pool_bck = self.bottleneck(avg_pool)
+        max_pool_bck = self.bottleneck(max_pool)
+
+        pool_sum = avg_pool_bck + max_pool_bck
+
+        sig_pool = torch.sigmoid(pool_sum)
+        sig_pool = sig_pool.unsqueeze(2).unsqueeze(3)
+
+        out = sig_pool.repeat(1,1,kernel[0], kernel[1])
+        return out
+
+
+
+## added C3 channel and spatial attention
+class C3Attn(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        """Initializes C3 module with options for channel count, bottleneck repetition, shortcut usage, group
+        convolutions, and expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        print(c1,c_,c2)
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.cbam=CBAM(n_channels_in=2*c_,reduction_ratio=2,kernel_size=3)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        """Performs forward propagation using concatenated outputs from two convolutions and a Bottleneck sequence."""
+        # return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+        return self.cbam(self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1)))
 
 class C3x(C3):
     # C3 module with cross-convolutions
@@ -444,7 +558,7 @@ class DetectMultiBackend(nn.Module):
         #   ONNX Runtime:                   *.onnx
         #   ONNX OpenCV DNN:                *.onnx --dnn
         #   OpenVINO:                       *_openvino_model
-        #   CoreML:                         *.mlpackage
+        #   CoreML:                         *.mlmodel
         #   TensorRT:                       *.engine
         #   TensorFlow SavedModel:          *_saved_model
         #   TensorFlow GraphDef:            *.pb
